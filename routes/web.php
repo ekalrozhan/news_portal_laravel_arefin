09c82e1d4a5e6d971afb356503ee529a46a29e01
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-08 20:30:45 +0800

    login, forget password template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
 
 
 
@@ -11,3 +12,5 @@ Route::get('/', function () {
 
 // Admin
 Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
+Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+Route::get('/admin/forget_password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
